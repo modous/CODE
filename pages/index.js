1fe59977746f6a5e866b96dc61a659cfd8dd8bd4
modous <mohamed1010@live.nl>
2024-01-17 11:52:07 +0100

    :+1:  De 2e alert button toegevoegd in index.js
    Ik had eerst geen alert op de 2e button wat zorgde voor een error pagina

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,29 @@
 // `app/page.js` is the UI for the `/` URL
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 import Button from "../components/Button/Button";
 import Layout from "../components/Layout/Layout";
 
+const client = new ApolloClient({
+  uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clph02cha0ml501t761bs521c/master",
+  cache: new InMemoryCache(),
+});
 
-
+console.log(client);
 
 export default function Home() {
   return (
     <>
       <Layout>
         <p>wa</p>
-        <Button onClick={() => alert("nee")}>klik</Button>
-        <Button onClick={"wat"}>niet klikken</Button>
+        <Button onClick={() => alert("Dit is Alert nummer 1")}>klik</Button>
+        <Button onClick={() => alert("Dit is een andere alert")}>
+          niet klikken
+        </Button>
       </Layout>
     </>
   );
